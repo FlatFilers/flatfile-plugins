02bfc132eecad564add58fd804a820fd272fc05c
Alex Hollenbeck <alex.hollenbeck@flatfile.io>
2023-05-12 14:54:58 -0500

    feat: tests

--- plugins/action/src/index.spec.ts
+++ plugins/action/src/index.spec.ts
@@ -1,27 +1,50 @@
 import { action } from '.'
-import {
-  Client,
-  FlatfileEvent,
-  FlatfileVirtualMachine,
-} from '@flatfile/listener'
+import { Client, FlatfileVirtualMachine } from '@flatfile/listener'
+
+const updateJobsFn = jest.fn()
+const testFn = jest.fn()
+
+jest.mock('@flatfile/api', () => ({
+  FlatfileClient: jest.fn().mockImplementation(() => {
+    return {
+      jobs: {
+        update: (jobId: string, payload: any) => updateJobsFn(jobId, payload),
+      },
+    }
+  }),
+}))
 
 describe('action', () => {
-  let testFn: jest.Mock
-
-  beforeEach(() => {
-    testFn = jest.fn()
-  })
-
-  test('it calls the passed function when action is triggered', () => {
-    const client = Client.create((c) => {
-      c.use(action('originSlug', 'actionName', testFn))
+  test('it calls the passed function and the jobs api when action is triggered', async () => {
+    const client = await Client.create((c) => {
+      c.use(action('duplicateOperation', testFn))
     })
-    const FlatfileVM = new FlatfileVirtualMachine()
-    client.mount(FlatfileVM)
-    client.dispatchEvent({
-      topic: 'action:triggered',
-      context: { actionName: 'originSlug:actionName' },
+    const FlatfileVM = await new FlatfileVirtualMachine()
+    await client.mount(FlatfileVM)
+    await client.dispatchEvent({
+      topic: 'job:created',
+      context: {
+        jobId: 'us_jb_nkP9PubO',
+      },
+      payload: {
+        operation: 'duplicateOperation',
+      },
     })
     expect(testFn).toHaveBeenCalledTimes(1)
+    expect(updateJobsFn).toHaveBeenCalledTimes(2)
+    expect(updateJobsFn.mock.calls).toEqual([
+      [
+        'us_jb_nkP9PubO',
+        {
+          status: 'executing',
+        },
+      ],
+      [
+        'us_jb_nkP9PubO',
+        {
+          status: 'complete',
+        },
+      ],
+    ])
   })
 })
