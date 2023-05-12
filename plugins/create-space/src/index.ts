887821a018dd9d229100357688ce1dc971107bf7
Alex Hollenbeck <alex.hollenbeck@flatfile.io>
2023-05-12 15:08:33 -0500

    feat: working plugin

--- plugins/create-space/src/index.ts
+++ plugins/create-space/src/index.ts
@@ -1,3 +1,22 @@
-export const createSpace = () => {
-  // To be implemented
+import { Client, FlatfileEvent } from '@flatfile/listener'
+import { FlatfileClient } from '@flatfile/api'
+
+export const createSpace = (
+  fn: (event: FlatfileEvent, api: FlatfileClient, jobId: string) => void
+) => {
+  const api = new FlatfileClient()
+  return (client: Client) => {
+    client.on('job:created', async (event: FlatfileEvent) => {
+      const { jobId } = event.context
+      if (event.payload.operation === 'configure') {
+        await api.jobs.update(jobId, {
+          status: 'executing',
+        })
+        await fn(event, api, jobId)
+        await api.jobs.update(jobId, {
+          status: 'complete',
+        })
+      }
+    })
+  }
 }
