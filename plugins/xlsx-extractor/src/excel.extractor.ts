77dd2a28d64a5a45487c263730fd5bbe32d8b99c
kamil <kamilnowakowski1533+github@gmail.com>
2023-06-18 17:37:02 +0200

    fix: introduce records chunking to xlsx extractor

--- plugins/xlsx-extractor/src/excel.extractor.ts
+++ plugins/xlsx-extractor/src/excel.extractor.ts
@@ -124,7 +124,9 @@ export class ExcelExtractor extends AbstractExtractor {
           continue;
         }
         const recordsData = this.makeAPIRecords(capture[sheet.name]);
-        await this.api.records.insert(sheet.id, recordsData);
+        await asyncBatch(recordsData, async (chunk) => {
+          await this.api.records.insert(sheet.id, chunk);
+        }, { chunkSize: 10000, parallel: 1 });
       }
       await this.completeJob(job);
       return true;
@@ -153,4 +155,61 @@ export class ExcelExtractor extends AbstractExtractor {
       typeof v === "string" ? /^[0-9]+$/.test(v) : !!v
     );
   }
+}
+
+async function asyncBatch<T, R>(
+  arr: T[],
+  callback: (chunk: T[]) => Promise<R>,
+  options: { chunkSize?: number; parallel?: number } = {}
+): Promise<R> {
+  const { chunkSize, parallel } = { chunkSize: 1000, parallel: 1, ...options }
+  const results: R[] = []
+
+  // Split the array into chunks
+  const chunks: T[][] = []
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    chunks.push(arr.slice(i, i + chunkSize))
+  }
+
+  // Create a helper function to process a chunk
+  async function processChunk(chunk: T[]): Promise<void> {
+    const result = await callback(chunk)
+    results.push(result)
+  }
+
+  // Execute the chunks in parallel
+  const promises: Promise<void>[] = []
+  let running = 0
+  let currentIndex = 0
+
+  function processNext(): void {
+    if (currentIndex >= chunks.length) {
+      // All chunks have been processed
+      return
+    }
+
+    const currentChunk = chunks[currentIndex]
+    const promise = processChunk(currentChunk).finally(() => {
+      running--
+      processNext() // Process next chunk
+    })
+
+    promises.push(promise)
+    currentIndex++
+    running++
+
+    if (running < parallel) {
+      processNext() // Process another chunk if available
+    }
+  }
+
+  // Start processing the chunks
+  for (let i = 0; i < parallel && i < chunks.length; i++) {
+    processNext()
+  }
+
+  // Wait for all promises to resolve
+  await Promise.all(promises)
+
+  return results.flat() as R
 }
