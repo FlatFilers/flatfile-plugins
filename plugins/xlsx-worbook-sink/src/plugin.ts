78cc97e98470631454d3cea7308ad7f465873907
colinfrederickson <110066636+colinfrederickson@users.noreply.github.com>
2023-07-10 17:01:32 -0400

    Introduces xlsxSinkPlugin and createXlsxWorkbook
    - adds dependencies for Plugin and function
    - adds index.ts
    - adds plugin.ts

--- plugins/xlsx-worbook-sink/src/plugin.ts
+++ plugins/xlsx-worbook-sink/src/plugin.ts
@@ -1,9 +1,19 @@
 import api, { Flatfile } from "@flatfile/api";
+import { FlatfileListener } from "@flatfile/listener";
+import { xlsxSinkPlugin } from "../src/index";
 
 /**
  * Some JSDOC here
  */
 export const run = () => {};
+
+export const createXlsxWorkbook = () => {
+  return (client: FlatfileListener) => {
+    client.on("workbook:downloadExcelWorkbook", (event) => {
+      return xlsxSinkPlugin();
+    });
+  };
+};
 
 const logError = (msg: string): void => {
   console.error("[@flatfile/plugin-xlsx-workbook-sink]:[FATAL]", msg);
