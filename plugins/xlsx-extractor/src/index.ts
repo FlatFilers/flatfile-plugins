2aaae2f228fbaf1e9b053858eb15c42e64d0235e
Carl Brugger <carl.brugger@flatfile.io>
2023-10-17 07:59:23 -0500

    feat: add raw option to Excel extractor (#262)
    * feat: add raw option to Excel extractor
    
    * Create sweet-bears-jump.md

--- plugins/xlsx-extractor/src/index.ts
+++ plugins/xlsx-extractor/src/index.ts
@@ -1,12 +1,24 @@
 import { Extractor } from '@flatfile/util-extractor'
 import { parseBuffer } from './parser'
 
-export const ExcelExtractor = (options?: {
-  rawNumbers?: boolean
-  chunkSize?: number
-  parallel?: number
-  debug?: boolean
-}) => {
+/**
+ * Plugin config options.
+ *
+ * @property {boolean} raw - if true, return raw data; if false, return formatted text.
+ * @property {boolean} rawNumbers - if true, return raw numbers; if false, return formatted numbers.
+ * @property {number} chunkSize - the size of chunk to process when inserting records.
+ * @property {number} parallel - the quantity of parallel process when inserting records.
+ * @property {boolean} debug - if true, display helpful console logs.
+ */
+export interface ExcelExtractorOptions {
+  readonly raw?: boolean
+  readonly rawNumbers?: boolean
+  readonly chunkSize?: number
+  readonly parallel?: number
+  readonly debug?: boolean
+}
+
+export const ExcelExtractor = (options?: ExcelExtractorOptions) => {
   return Extractor(
     /\.(xlsx?|xlsm|xlsb|xltx?|xltm)$/i,
     'excel',
