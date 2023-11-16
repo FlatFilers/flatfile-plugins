0d5f2c11652dd18327bc2b0ea2d21ce85155386f
Morgan Daley <61160473+madaley1@users.noreply.github.com>
2023-11-16 09:05:16 -0700

    Configure Space with YAML Plugin (#312)
    * initial commit for YAML Schema Plugin
    
    * add dependency
    
    * fix: remove changes to JSON-schema meant for YAML-schema
    
    * fix: changeset
    
    * fix: improper changeset
    
    * fix: restore json-schema readme
    
    * fix: remove console.dir

---
+++ utils/fetch-schema/src/index.ts
@@ -0,0 +1,56 @@
+import axios from 'axios'
+
+import { Flatfile } from '@flatfile/api'
+
+export interface ModelToSheetConfig extends PartialSheetConfig {
+  sourceUrl: string
+}
+
+export type PartialSheetConfig = Omit<
+  Flatfile.SheetConfig,
+  'fields' | 'name'
+> & {
+  name?: string
+}
+
+export type PartialWorkbookConfig = Omit<
+  Flatfile.CreateWorkbookConfig,
+  'sheets' | 'name'
+> & {
+  name?: string
+}
+
+export async function getSchemas(models?: ModelToSheetConfig[]) {
+  const schemas = models.map(async (model: ModelToSheetConfig) => {
+    return await fetchExternalReference(model.sourceUrl)
+  })
+
+  return await Promise.all(schemas)
+}
+
+export async function fetchExternalReference(url: string): Promise<any> {
+  try {
+    const { status, data } = await axios.get(url, {
+      validateStatus: () => true,
+    })
+    if (status !== 200)
+      throw new Error(`API returned status ${status}: ${data.statusText}`)
+    return data
+  } catch (error: any) {
+    throw new Error(`Error fetching external reference: ${error.message}`)
+  }
+}
+
+export function resolveLocalReference(schema: any, ref: string): any {
+  const resolved = ref
+    .split('/')
+    .slice(1)
+    .reduce(
+      (acc, part) =>
+        acc && (acc[part] || acc.$defs?.[part] || acc.definitions?.[part]),
+      schema
+    )
+
+  if (!resolved) throw new Error(`Cannot resolve reference: ${ref}`)
+  return resolved
+}
