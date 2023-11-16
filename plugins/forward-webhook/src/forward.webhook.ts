f2c970cd0601e2e6744c46da6172954e57616fb8
madaley1 <mor2daley@gmail.com>
2023-11-16 07:04:49 -0700

    remove debug code

--- plugins/forward-webhook/src/forward.webhook.ts
+++ plugins/forward-webhook/src/forward.webhook.ts
@@ -12,13 +12,9 @@ export function forwardWebhook(
   return async (listener: FlatfileListener) => {
     return listener.on('**', async (e) => {
       if (e.topic === 'job:outcome-acknowledged') return
-      // console.dir(e, { depth: null })
       const { data } = await axios.post(url, { ...e })
       let callbackData = undefined
       if (callback) callbackData = await callback(data)
-      // ('forward-webhook', callbackData || data)
-      console.log('forward-webhook')
-      // console.dir(callbackData || data, { depth: null })
       api.events.create({
         domain: e.domain as Flatfile.Domain,
         topic: 'job:outcome-acknowledged',
