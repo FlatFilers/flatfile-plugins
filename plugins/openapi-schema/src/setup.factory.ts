5981817a4bba7d0563e859d8570869eb93127507
Carl Brugger <carl.brugger@flatfile.io>
2023-11-24 12:55:00 -0600

    feat: support more field types

--- plugins/openapi-schema/src/setup.factory.ts
+++ plugins/openapi-schema/src/setup.factory.ts
@@ -31,56 +31,6 @@ interface ApiSchemas {
   [key: string]: OpenApiSchema
 }
 
-const convertPropertyToField = (
-  key: string,
-  property: OpenApiSchema,
-  schemas: ApiSchemas,
-  options?: { debug?: boolean }
-): Flatfile.Property => {
-  const baseField: Flatfile.BaseProperty = {
-    key,
-    label: property.title || key,
-    description: `${property.description}`,
-    readonly: property.readOnly || false,
-    constraints: [],
-  }
-
-  const refName = property.$ref || property.allOf?.[0].$ref?.split('/').pop()
-  if (refName) {
-    property = schemas[refName]
-  }
-
-  switch (property.type) {
-    case 'string':
-      if (property.enum) {
-        return {
-          ...baseField,
-          type: 'enum',
-          config: {
-            options: property.enum.map((e) => ({ label: e, value: e })),
-          },
-        } as Flatfile.Property.Enum
-      }
-      return { ...baseField, type: 'string' } as Flatfile.Property.String
-    case 'integer':
-    case 'number':
-      return {
-        ...baseField,
-        type: 'number',
-        config: { decimalPlaces: 0 },
-      } as Flatfile.Property.Number
-    case 'boolean':
-      return { ...baseField, type: 'boolean' } as Flatfile.Property.Boolean
-    default:
-      if (options?.debug) {
-        console.log(
-          `Unsupported field '${baseField.label}' with property type '${property.type}'.`
-        )
-      }
-      return
-  }
-}
-
 export async function generateSetup(
   url: string,
   options?: {
@@ -100,40 +50,28 @@ export async function generateSetup(
 
     const schemas: ApiSchemas = data.components.schemas
 
-    const sheetConfigs: Flatfile.SheetConfig[] = Object.entries(schemas)
-      .filter(
-        ([key]) => !options?.models || options?.models.hasOwnProperty(key)
-      )
-      .map(([key, schema]) => {
-        const fields: Flatfile.Property[] = Object.entries(
-          schema.properties || {}
+    const sheetConfigs: Flatfile.SheetConfig[] = await Promise.all(
+      Object.entries(schemas)
+        .filter(
+          ([key]) => !options?.models || options?.models.hasOwnProperty(key)
         )
-          .map(([propKey, property]) =>
-            convertPropertyToField(propKey, property, schemas)
-          )
-          .filter(Boolean)
-
-        const requiredFields = new Set(schema.required || [])
-        fields.forEach((field) => {
-          if (requiredFields.has(field.key)) {
-            field.constraints?.push({ type: 'required' })
+        .map(async ([key, schema]) => {
+          const fields = await generateFields(schema, schemas)
+          const modelDetails = options?.models?.[key]
+          return {
+            name: modelDetails?.name || key,
+            fields,
+            ...modelDetails,
           }
         })
-
-        const modelDetails = options?.models?.[key]
-        return {
-          name: modelDetails?.name || key,
-          fields,
-          ...modelDetails,
-        }
-      })
+    )
 
     return {
       workbooks: [
         {
           name: data.info.title,
-          sheets: sheetConfigs,
           ...options?.workbookConfig,
+          sheets: sheetConfigs,
         },
       ],
     }
@@ -141,4 +79,151 @@ export async function generateSetup(
     console.error(error)
     throw new Error(`Error fetching or processing schema: ${error.message}`)
   }
+}
+
+export async function generateFields(
+  data: any,
+  origin: any
+): Promise<Flatfile.Property[]> {
+  if (!data.properties) return []
+
+  const fields = await Promise.all(
+    Object.keys(data.properties).map((key) =>
+      getPropertyType(
+        data,
+        data.properties[key],
+        key,
+        (data.required && data.required.includes(key)) || false,
+        origin
+      )
+    )
+  )
+  return fields.flat().filter(Boolean)
+}
+
+export async function getPropertyType(
+  schema: any,
+  property: any,
+  parentKey = '',
+  isRequired = false,
+  origin: string
+): Promise<Flatfile.Property[]> {
+  if (property.$ref) {
+    return getPropertyType(
+      schema,
+      await resolveReference(origin, property.$ref),
+      parentKey,
+      false,
+      origin
+    )
+  }
+
+  if (property.type === 'object' && property.properties) {
+    return (
+      await Promise.all(
+        Object.keys(property.properties).map(async (key) => {
+          return getPropertyType(
+            property,
+            property.properties[key],
+            parentKey ? `${parentKey}_${key}` : key,
+            (property.required && property.required.includes(key)) || false,
+            origin
+          )
+        })
+      )
+    ).flat()
+  }
+
+  if (property.allOf) {
+    // Handle allOf construct
+    let combinedProperty = {}
+    for (const prop of property.allOf) {
+      if (prop.$ref) {
+        const resolved = await resolveReference(origin, prop.$ref)
+        combinedProperty = { ...combinedProperty, ...resolved }
+      } else {
+        combinedProperty = { ...combinedProperty, ...prop }
+      }
+    }
+    return getPropertyType(
+      schema,
+      combinedProperty,
+      parentKey,
+      isRequired,
+      origin
+    )
+  }
+
+  if (property.type === 'string' && property.enum) {
+    return [
+      {
+        key: parentKey,
+        type: 'enum',
+        label: parentKey,
+        ...(property?.description && { description: property.description }),
+        ...(isRequired && { constraints: [{ type: 'required' }] }),
+        config: {
+          options: property.enum.map((value) => ({
+            value,
+            label: String(value),
+          })),
+        },
+      },
+    ]
+  }
+
+  const fieldTypes: Record<string, Flatfile.Property> = {
+    string: { key: parentKey, type: 'string' },
+    number: { key: parentKey, type: 'number' },
+    integer: { key: parentKey, type: 'number' },
+    boolean: { key: parentKey, type: 'boolean' },
+    array: {
+      key: parentKey,
+      type: 'enum',
+      description: 'An enum of Selected Values',
+      config: property.enum
+        ? {
+            options: property.enum.map((value: any) => ({
+              value,
+              label: String(value),
+            })),
+          }
+        : {
+            options: [],
+          },
+    },
+    enum: {
+      key: parentKey,
+      type: 'enum',
+      config: property?.enum
+        ? {
+            options: property.enum.map((value: any) => ({
+              value,
+              label: String(value),
+            })),
+          }
+        : {
+            options: [],
+          },
+    },
+  }
+
+  const fieldConfig: Flatfile.Property = {
+    label: parentKey,
+    ...(property?.description && { description: property.description }),
+    ...(isRequired && { constraints: [{ type: 'required' }] }),
+    ...fieldTypes[property.type],
+  }
+
+  return fieldTypes[fieldConfig.type] ? [fieldConfig] : []
+}
+
+export async function resolveReference(schema: any, ref: string): Promise<any> {
+  const segments = ref.split('/')
+  const modelName = segments[segments.length - 1]
+
+  const model = schema[modelName]
+
+  if (!model) throw new Error(`Cannot resolve reference: ${ref}`)
+  return model
 }
