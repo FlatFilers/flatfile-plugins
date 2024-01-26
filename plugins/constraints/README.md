51bcd787955c9e8611a6e3738237b649f9ab47b6
David Boskovic <david@flatfile.io>
2024-01-25 23:46:18 -0500

    feat: external constraints plugin

---
+++ plugins/constraints/README.md
@@ -0,0 +1,3 @@
+# @flatfile/plugin-constraints
+
+[todo]
---
+++ plugins/constraints/src/external.constraint.ts
@@ -0,0 +1,53 @@
+import api, { Flatfile } from '@flatfile/api'
+import { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
+import { FlatfileRecord } from '@flatfile/hooks'
+import { bulkRecordHookPlugin } from '@flatfile/plugin-record-hook'
+import { getConstraints } from './get.constraints'
+
+export const externalConstraint = (
+  validator: string,
+  cb: (
+    value: any,
+    key: string,
+    support: {
+      config: any
+      record: FlatfileRecord
+      property: Flatfile.Property
+      event: FlatfileEvent
+    }
+  ) => any
+) => {
+  return (listener: FlatfileListener) => {
+    // setup the cache
+    // todo: cache this schema longer than one event
+    listener.on('commit:created', async (e) => {
+      await e.cache.init('sheet-schema', async () => {
+        const res = await api.sheets.get(e.context.sheetId)
+        return res.data.config
+      })
+    })
+
+    listener.use(
+      bulkRecordHookPlugin('**', async (records, event) => {
+        const schema: Flatfile.SheetConfig = event.cache.get('sheet-schema')
+
+        const constraints = getConstraints(schema, validator)
+
+        constraints.forEach(([property, constraint]) => {
+          records.forEach((record) => {
+            try {
+              cb(record.get(property.key), property.key, {
+                config: constraint.config,
+                record,
+                property,
+                event,
+              })
+            } catch (e) {
+              record.addError(property.key, String(e))
+            }
+          })
+        })
+      })
+    )
+  }
+}
