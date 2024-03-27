5f476d0c3e844ae952f83acb79584626b6808cd7
Carl Brugger <carl.brugger@flatfile.io>
2024-03-27 11:21:03 -0500

    feat: tests & improved reference field key selection

--- plugins/graphql-schema/src/generate.field.ts
+++ plugins/graphql-schema/src/generate.field.ts
@@ -1,23 +1,29 @@
 import type { Flatfile } from '@flatfile/api'
 
-export function generateField(field, sheetName): Flatfile.Property {
-  const baseProperty = {
-    key: field.name,
-    label: field.name,
-    description: field.description || '',
-    constraints: field.type.kind === 'NON_NULL' ? [{ type: 'required' }] : [],
-  } as Flatfile.BaseProperty
+export function generateField(
+  field,
+  sheetName: string,
+  baseProperty?: Flatfile.BaseProperty
+): Flatfile.Property {
+  const base =
+    baseProperty ||
+    ({
+      key: field.name,
+      label: field.name,
+      description: field.description || '',
+      constraints: field.type.kind === 'NON_NULL' ? [{ type: 'required' }] : [],
+    } as Flatfile.BaseProperty)
 
   if (field.type.kind === 'SCALAR') {
-    return { ...baseProperty, type: getScalarType(field.type.name) }
+    return { ...base, type: getScalarType(field.type.name) }
   } else if (field.type.kind === 'OBJECT') {
     return {
-      ...baseProperty,
+      ...base,
       type: 'reference',
       config: { ref: field.type.name, key: 'id', relationship: 'has-one' },
     }
   } else if (field.type.kind === 'NON_NULL') {
-    return generateField({ ...field, type: field.type.ofType }, sheetName)
+    return generateField({ ...field, type: field.type.ofType }, sheetName, base)
   } else {
     console.log(
       `Field '${field.name}' on '${sheetName}' skipped because '${field.type.kind}' is unsupported.`
--- plugins/graphql-schema/src/generate.sheet.ts
+++ plugins/graphql-schema/src/generate.sheet.ts
@@ -1,12 +1,13 @@
 import type { Flatfile } from '@flatfile/api'
 import { capitalCase } from 'change-case'
 import { generateField } from './generate.field'
+import { PartialSheetConfig } from './types'
 
 export function generateSheets(
   graphQLObjects,
-  sheetConfigArray
+  sheetConfigArray: PartialSheetConfig[]
 ): Flatfile.SheetConfig[] {
-  return graphQLObjects
+  const sheets = graphQLObjects
     .map((object) => {
       let sheetConfig =
         sheetConfigArray?.find((config) => config.slug === object.name) || {}
@@ -14,9 +15,6 @@ export function generateSheets(
       const fields = object.fields
         .map((field) => generateField(field, object.name))
         .filter(Boolean)
-      if (!fields.some((field) => field.key === 'id')) {
-        fields.unshift({ key: 'id', label: 'Id', type: 'number' })
-      }
 
       return fields.length
         ? {
@@ -38,4 +36,38 @@ export function generateSheets(
               graphQLObjects.some((obj) => obj.name === field.config.ref)
           ))
     )
+
+  sheets.forEach((sheet) => {
+    sheet.fields = sheet.fields.filter((field) => {
+      if (field.type !== 'reference') {
+        return true
+      }
+
+      const refSheet = sheets.find((s) => s.slug === field.config.ref)
+      if (!refSheet) {
+        console.warn(
+          `Reference table '${field.config.ref}' not found. Removing field '${field.label}' from '${sheet.name}'.`
+        )
+        return false
+      }
+
+      field.config.key = refSheet.fields.find(
+        (f) => f.type !== 'reference'
+      )?.key
+
+      if (!field.config.key) {
+        console.warn(
+          `Reference table '${refSheet.name}' does not contain a field that can be referenced by '${field.label}'. Removing field '${field.label}' from '${sheet.name}'.`
+        )
+        return false
+      }
+
+      console.log(
+        `A valid reference field found for '${field.label}' in '${sheet.name}'.`
+      )
+      return true
+    })
+  })
+
+  return sheets
 }
