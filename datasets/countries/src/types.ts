fa190614d14e28d61e25696533122d33d96a1a75
Carl Brugger <carl.brugger@flatfile.io>
2024-04-04 22:47:10 -0500

    feat: dataset plugins

---
+++ datasets/countries/src/types.ts
@@ -0,0 +1,20 @@
+export type CountryData = {
+  iso_a2: string
+  iso3: string
+  iso_numeric: string
+  fips: string
+  impact_country: string
+  capital: string
+  area: number
+  population: number
+  continent: string
+  tld: string
+  currency_code: string
+  currency_name: string
+  phone: string
+  postal_code_format: string
+  postal_code_regex: string
+  languages: string[]
+  geonameid: number
+  neighbours: string
+}
