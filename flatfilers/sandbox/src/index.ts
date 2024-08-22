4689f813b7c7b579b8d4c0a64f6cc925c1290758
damonbanks <damon.banks@flatfile.io>
2024-08-22 11:23:38 -0600

    updated listern to job:updated and waiting for job status

--- flatfilers/sandbox/src/index.ts
+++ flatfilers/sandbox/src/index.ts
@@ -1,8 +1,25 @@
-import type { FlatfileListener } from '@flatfile/listener'
+import type { FlatfileListener, FlatfileEvent } from '@flatfile/listener'
 import { ExcelExtractor } from '@flatfile/plugin-xlsx-extractor'
 import { DelimiterExtractor } from '@flatfile/plugin-delimiter-extractor'
+import { automap } from "@flatfile/plugin-automap";
+
 
 export default async function (listener: FlatfileListener) {
   listener.use(ExcelExtractor())
   listener.use(DelimiterExtractor('txt', { delimiter: ',' }))
+
+  listener.use(
+    automap({
+      accuracy: "confident",
+      defaultTargetSheet: "contacts",
+      matchFilename: /test/,
+      debug: true,
+      onFailure: (event: FlatfileEvent) => {
+        // send an SMS, an email, post to an endpoint, etc.
+        console.error(
+          `Please visit https://spaces.flatfile.com/space/${event.context.spaceId}/files?mode=import to manually import file.`
+        );
+      },
+    })
+  );
 }
