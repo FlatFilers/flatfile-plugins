cce7aed55836c3714211690e22634d907d2564f4
Alex Rock <hi@lxrck.com>
2024-09-23 17:43:30 -0600

    feat: simplify

--- validate/isbn/rollup.config.mjs
+++ validate/isbn/rollup.config.mjs
@@ -1,35 +1,14 @@
-import { buildConfig } from '@flatfile/rollup-config';
-import typescript from '@rollup/plugin-typescript';
+import { buildConfig } from '@flatfile/rollup-config'
 
 const umdExternals = [
   '@flatfile/api',
   '@flatfile/plugin-record-hook',
   '@flatfile/listener',
-];
+]
 
 const config = buildConfig({
   includeUmd: true,
-  umdConfig: { name: 'flatfileIsbnPlugin', external: umdExternals },
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist',
-    }),
-  ],
-});
+  umdConfig: { name: 'FlatfileISBNValidator', external: umdExternals },
+})
 
-// Ensure all builds output TypeScript declarations
-config.forEach(conf => {
-  if (conf.plugins) {
-    conf.plugins.push(
-      typescript({
-        tsconfig: './tsconfig.json',
-        declaration: true,
-        declarationDir: 'dist',
-      })
-    );
-  }
-});
-
-export default config;
+export default config
