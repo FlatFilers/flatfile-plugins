67f01aeeff96d1755ccedf8c1b52e34fad935ec2
Alex Rock <hi@lxrck.com>
2024-09-30 22:10:38 -0600

    Apply suggestions from code review
    Co-authored-by: Carl Brugger <carl.brugger@flatfile.io>

--- validate/isbn/rollup.config.mjs
+++ validate/isbn/rollup.config.mjs
@@ -1,15 +1,5 @@
 import { buildConfig } from '@flatfile/rollup-config';
 
-export default buildConfig({
-  external: ['@flatfile/listener', '@flatfile/plugin-record-hook', 'isbn3'],
-  includeBrowser: true,
-  includeUmd: true,
-  umdConfig: {
-    name: 'FlatfileIsbnValidator',
-    globals: {
-      '@flatfile/listener': 'FlatfileListener',
-      '@flatfile/plugin-record-hook': 'FlatfilePluginRecordHook',
-      'isbn3': 'ISBN'
-    }
-  }
-});
+const config = buildConfig({});
+
+export default config;
