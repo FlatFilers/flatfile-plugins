55e052ac52cc41ff9988c1d9c91f3978fcf16b94
Alex Rock <hi@lxrck.com>
2024-10-03 15:54:54 -0600

    feat: jspdf instead of pdfkit

--- validators/PDFReportGenerator/rollup.config.mjs
+++ validators/PDFReportGenerator/rollup.config.mjs
@@ -1,45 +1,13 @@
 import { buildConfig } from '@flatfile/rollup-config';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import json from '@rollup/plugin-json';
 
-const umdExternals = [
-  '@flatfile/api',
-  '@flatfile/hooks',
-  '@flatfile/listener',
-  '@flatfile/util-common',
-  '@flatfile/plugin-space-configure',
-  'pdfkit',
-  'chartjs-node-canvas'
-];
-
-const config = buildConfig({
-  input: 'src/index.ts', // Assuming your main file is index.ts in the src directory
+export default buildConfig({
   external: [
-    ...umdExternals,
-    'fs',
-    'path'
+    '@flatfile/listener',
+    '@flatfile/plugin-record-hook',
+    'jspdf',
+    'jspdf-autotable',
+    'chart.js/auto',
+    '@flatfile/api'
   ],
-  includeBrowser: true,
-  includeUmd: true,
-  umdConfig: { 
-    name: 'FlatfilePDFGenerator', 
-    external: umdExternals 
-  },
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist/types',
-    }),
-    commonjs(),
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    json(),
-  ]
+  includeBrowser: true
 });
-
-export default config;
