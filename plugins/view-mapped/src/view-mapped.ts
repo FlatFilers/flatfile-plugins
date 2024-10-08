4ea752dcaed8a7006d92d7e8c52b3fb3b9b231cf
Carl Brugger <carl.brugger@flatfile.io>
2024-10-08 08:44:07 -0400

    refactor

--- plugins/view-mapped/src/view-mapped.ts
+++ plugins/view-mapped/src/view-mapped.ts
@@ -1,12 +1,15 @@
-import api from '@flatfile/api'
+import { FlatfileClient } from '@flatfile/api'
 import type { FlatfileListener } from '@flatfile/listener'
+import { jobHandler } from '@flatfile/plugin-job-handler'
+import { logError } from '@flatfile/util-common'
+
+const api = new FlatfileClient()
 
 /**
  * This plugin allows you to make the post-mapping sheet only display mapped data
  */
 export function viewMappedPlugin() {
   return (listener: FlatfileListener) => {
-    
     // Defining what needs to be done when Flatfile is done mapping columns based on user input during the Mapping stage of the import process
     listener.on(
       'job:completed',
@@ -14,30 +17,27 @@ export function viewMappedPlugin() {
       async ({ context: { jobId, workbookId } }) => {
         // Creating a custom job that we will use in the next listener to ensure users only see mapped fields in the table
         await api.jobs.create({
-          type: "workbook",
-          operation: "viewMappedFieldsOnly",
+          type: 'workbook',
+          operation: 'viewMappedFieldsOnly',
           source: workbookId,
           // This ensures that our custom job will execute automatically when the "job:ready" event of the listener below triggers
-          trigger: "immediate",
+          trigger: 'immediate',
           // This ensures that users are not able to interact with records in the table until it is updated to only show mapped fields
-          mode: "foreground",
+          mode: 'foreground',
           // This ensures that in the next listener we are able to access the jobId of the mapping job specifically, and not just the jobId of this custom job
-          input: { mappingJobId: jobId }
+          input: { mappingJobId: jobId },
         })
       }
     )
 
     // Defining what needs to be done when our custom job triggers. Because we create it when mapping job completes, this is when this job will begin executing
-    listener
-      .filter({ job: 'workbook:viewMappedFieldsOnly' })
-      .on('job:ready', async ({ context: { jobId, workbookId } }) => {
-        
+    listener.use(
+      jobHandler('workbook:viewMappedFieldsOnly', async (event, tick) => {
+        const { jobId, workbookId } = event.context
+
         try {
           // First, we acknowledge the job
-          await api.jobs.ack(jobId, {
-            info: 'Updating the table to only view mapped fields',
-            progress: 10,
-          })
+          await tick(10, 'Updating the table to only view mapped fields')
 
           // Retrieving the info on the custom job we created in the listener above, and storing that info in its own "customJobInfo" variable
           const customJobInfo = await api.jobs.get(jobId)
@@ -59,10 +59,7 @@ export function viewMappedPlugin() {
             mappedFields.push(destinationFieldKey)
           }
 
-          await api.jobs.ack(jobId, {
-            info: "Updating the table to only view mapped fields...",
-            progress: 30
-          })
+          await tick(30, 'Updating the table to only view mapped fields')
 
           // Making an API call to only get the "data" property out of the response, and saving it as its own "fetchedWorkbook" variable
           // We need to make this API call and cannot just use what's inside of "workbookOne" because we need data in a specific format
@@ -85,10 +82,27 @@ export function viewMappedPlugin() {
             return fields.length > 0 ? fields : null
           })
 
-          await api.jobs.ack(jobId, {
-            info: "Halfway there, hang tight...",
-            progress: 50
+          await tick(50, 'Halfway there, hang tight...')
+
+          const sheets = workbook.sheets.map((sheet, index) => {
+            const mappedWorkbookFields = filteredWorkbookFields[index]
+
+            // If there are no mapped fields, returning the original sheet structure
+            if (!mappedWorkbookFields) {
+              return sheet
+            }
+
+            // If there are mapped fields, returning all properties of the original sheet but updating the "fields" property to the mapped fields
+            return {
+              ...sheet,
+              config: {
+                ...sheet.config,
+                fields: mappedWorkbookFields,
+              },
+            }
           })
+
+          await tick(80, 'Almost done...')
 
           // Updating each sheet in a workbook to only contain fields that a user mapped. This ensures that when the table with data loads, only mapped fields will be displayed
           await api.workbooks.update(workbookId, {
@@ -96,47 +110,27 @@ export function viewMappedPlugin() {
             ...workbook,
 
             // Mapping over each sheet to update each to only contain fields that are inside of "filteredWorkbookFields" variable (that have metadata "{mapped: true})"
-            sheets: workbook.sheets.map((sheet, index) => {
-              const mappedWorkbookFields = filteredWorkbookFields[index]
-
-              // If there are no mapped fields, returning the original sheet structure
-              if (!mappedWorkbookFields) {
-                return sheet
-              }
-
-              // If there are mapped fields, returning all properties of the original sheet but updating the "fields" property to the mapped fields
-              return {
-                ...sheet,
-                config: {
-                  ...sheet.config,
-                  fields: mappedWorkbookFields,
-                },
-              }
-            }),
-          })
-
-          await api.jobs.ack(jobId, {
-            info: "Almost done...",
-            progress: 80
+            sheets,
           })
 
           // Completing the job with an appropriate message to the user
-          await api.jobs.complete(jobId, {
+          return {
             outcome: {
-              message: "Table update complete. Please audit the data",
-              acknowledge: false
-            }
-          })
-
+              message: 'Table update complete. Please audit the data',
+              acknowledge: false,
+            },
+          }
         } catch (error) {
           // If something goes wrong while executing the custom job, we fail the job with a message on what next steps to take
-          await api.jobs.fail(jobId, {
-            outcome: {
-              message:
-                'An error occured while updating the workbook. See Event Logs.',
-            },
-          })
+          logError(
+            '@flatfile/plugin-view-mapped',
+            JSON.stringify(error, null, 2)
+          )
+          throw new Error(
+            'An error occured while updating the workbook. See Event Logs.'
+          )
         }
       })
+    )
   }
 }
