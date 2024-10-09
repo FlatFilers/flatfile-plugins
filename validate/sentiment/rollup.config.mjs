709b8081328f96f499e46f8fe23f7cef42e29fbc
Alex Rock <hi@lxrck.com>
2024-10-09 14:04:55 -0400

    feat: simplify rollup.config

--- validate/sentiment/rollup.config.mjs
+++ validate/sentiment/rollup.config.mjs
@@ -1,36 +1,5 @@
-import { buildConfig } from '@flatfile/rollup-config';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import { buildConfig } from '@flatfile/rollup-config'
 
-const umdExternals = [
-  '@flatfile/api',
-  '@flatfile/hooks',
-  '@flatfile/listener',
-  '@flatfile/util-common',
-  '@flatfile/plugin-record-hook',
-  'sentiment'
-];
+const config = buildConfig({})
 
-const config = buildConfig({
-  input: 'src/index.ts', // Assuming your main file is src/index.ts
-  includeUmd: true,
-  umdConfig: { 
-    name: 'FlatfileSentimentAnalysisPlugin', 
-    external: umdExternals 
-  },
-  plugins: [
-    resolve({
-      browser: true,
-      preferBuiltins: false,
-    }),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: './dist/types',
-    }),
-  ],
-});
-
-export default config;
+export default config
