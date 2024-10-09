c84dea6cf37668094b59d209b4152886df678fd7
Alex Rock <hi@lxrck.com>
2024-10-09 15:44:31 -0400

    validator: string (#650)
    * koala: initial commit
    
    * feat: cleanup and test
    
    * feat: cleanup
    
    * feat: Readme updates
    
    * feat: refactor to unit tests
    
    * feat: update package.json
    
    * chore: remove metadata.json
    
    * feat: update test scripts
    
    * feat: install deps
    
    * feat: jest.config.js
    
    * feat: jest.config.js
    
    * feat: fix phone test

---
+++ validate/string/src/validate.string.plugin.ts
@@ -0,0 +1,33 @@
+import { FlatfileListener } from '@flatfile/listener'
+import { recordHook } from '@flatfile/plugin-record-hook'
+import {
+  StringValidationConfig,
+  validateAndTransformString,
+} from './validate.string.utils'
+
+export function validateString(config: StringValidationConfig) {
+  return (listener: FlatfileListener) => {
+    listener.use(
+      recordHook(config.sheetSlug || '**', (record) => {
+        for (const field of config.fields) {
+          const value = record.get(field) as string
+          if (value !== null && value !== undefined) {
+            const { value: newValue, error } = validateAndTransformString(
+              value,
+              config
+            )
+            if (error) {
+              record.addError(field, error)
+            }
+            if (newValue !== value) {
+              record.set(field, newValue)
+            }
+          }
+        }
+        return record
+      })
+    )
+  }
+}
+
+export default validateString
