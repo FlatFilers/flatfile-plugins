8865c065fb7a0e2d0226aec2dbe5c97f75feefd8
Carl Brugger <carl.brugger@flatfile.io>
2024-10-23 01:20:15 -0500

    new plugins -> tsup

--- extract/html-table/rollup.config.mjs
+++
@@ -1,5 +0,0 @@
-import { buildConfig } from '@flatfile/bundler-config-rollup'
-
-const config = buildConfig({})
-
-export default config
--- validate/email/rollup.config.mjs
+++
@@ -1,7 +0,0 @@
-import { buildConfig } from '@flatfile/bundler-config-rollup'
-
-const external = ['@flatfile/plugin-record-hook']
-
-const config = buildConfig({ external })
-
-export default config
