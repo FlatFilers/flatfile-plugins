366f5e4e77a15cb2b73220a0a07d698a182e0a9f
Carl Brugger <carl.brugger@flatfile.io>
2024-10-24 09:53:41 -0500

    add UMD support & remove parcel deps

--- bundlers/tsup-config/index.mjs
+++ bundlers/tsup-config/index.mjs
@@ -3,39 +3,71 @@ import { defineConfig as tsupDefineConfig } from 'tsup'
 
 dotenv.config()
 
-const PROD = process.env.NODE_ENV === 'production'
-if (!PROD) {
-  console.log('Not in production mode - skipping minification')
-}
+export function defineConfig({
+  includeBrowser = true,
+  includeNode = true,
+  includeUmd = false,
+  umdConfig = { name: undefined, external: [] },
+}) {
+  const minify = process.env.NODE_ENV === 'production'
+  if (!minify) {
+    console.log('Not in production mode - skipping minification')
+  }
 
-const nodeConfig = {
-  name: 'node',
-  platform: 'node',
-  minify: process.env.NODE_ENV === 'production',
-  entryPoints: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  outDir: 'dist',
-  clean: true,
-  outExtension: ({ format }) => ({
-    js: format === 'cjs' ? '.cjs' : '.js',
-  }),
-}
+  const nodeConfig = {
+    name: 'node',
+    platform: 'node',
+    minify,
+    entryPoints: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    outDir: 'dist',
+    clean: true,
+    outExtension: ({ format }) => ({
+      js: format === 'cjs' ? '.cjs' : '.js',
+    }),
+  }
 
-const browserConfig = {
-  name: 'browser',
-  platform: 'browser',
-  minify: process.env.NODE_ENV === 'production',
-  entryPoints: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  outDir: 'dist',
-  clean: true,
-  outExtension: ({ format }) => ({
-    js: format === 'cjs' ? '.browser.cjs' : '.browser.js',
-  }),
-}
+  if (includeUmd) {
+    nodeConfig.format.push('iife')
+    nodeConfig.globalName = umdConfig.name
+    nodeConfig.outExtension = ({ format }) => ({
+      js: format === 'cjs' ? '.cjs' : format === 'iife' ? '.umd.js' : '.js',
+    })
+    nodeConfig.external = umdConfig.external
+  }
 
-export function defineConfig({ includeBrowser = true }) {
-  return tsupDefineConfig(includeBrowser ? [nodeConfig, browserConfig] : [nodeConfig])
+  const browserConfig = {
+    name: 'browser',
+    platform: 'browser',
+    minify,
+    entryPoints: ['src/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    outDir: 'dist',
+    clean: true,
+    outExtension: ({ format }) => ({
+      js: format === 'cjs' ? '.browser.cjs' : '.browser.js',
+    }),
+  }
+
+  if (includeUmd) {
+    browserConfig.format.push('iife')
+    browserConfig.globalName = umdConfig.name
+    browserConfig.outExtension = ({ format }) => ({
+      js:
+        format === 'cjs'
+          ? '.browser.cjs'
+          : format === 'iife'
+            ? '.browser.umd.js'
+            : '.browser.js',
+    })
+    browserConfig.external = umdConfig.external
+  }
+
+  const configs = []
+  if (includeNode) configs.push(nodeConfig)
+  if (includeBrowser) configs.push(browserConfig)
+
+  return tsupDefineConfig(configs)
 }
