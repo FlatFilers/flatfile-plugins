24262ec5be8e45268b308e82d723035303d5da39
Carl Brugger <carl.brugger@flatfile.io>
2024-10-24 09:53:41 -0500

    add UMD support & remove parcel deps

--- plugins/record-hook/tsup.config.mjs
+++ plugins/record-hook/tsup.config.mjs
@@ -1,3 +1,14 @@
 import { defineConfig } from '@flatfile/bundler-config-tsup'
 
-export default defineConfig({})
+const umdExternals = [
+  '@flatfile/api',
+  '@flatfile/hooks',
+  '@flatfile/listener',
+  '@flatfile/util-common',
+]
+
+export default defineConfig({
+  globalName: 'PluginRecordHook',
+  includeUmd: true,
+  umdConfig: { name: 'PluginRecordHook', external: umdExternals },
+})
