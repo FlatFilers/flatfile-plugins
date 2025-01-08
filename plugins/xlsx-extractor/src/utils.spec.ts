62d2bf3750f3b6e6afeaec0a9e2ca6b85641b2a3
Carl Brugger <carl.brugger@flatfile.io>
2025-01-08 14:32:56 -0600

    fix: issue 1699 (#729)
    * fix: issue 1699
    
    * Trim headers appropriately
    
    * Simplify logic
    
    * Update test

--- plugins/xlsx-extractor/src/utils.spec.ts
+++ plugins/xlsx-extractor/src/utils.spec.ts
@@ -4,49 +4,43 @@ import { prependNonUniqueHeaderColumns } from './utils'
 describe('utils', () => {
   describe('prependNonUniqueHeaderColumns', () => {
     it('should handle unique values correctly', () => {
-      const input = { a: 'value1', b: 'value2', c: 'value3' }
-      const expected = { a: 'value1', b: 'value2', c: 'value3' }
+      const input = ['value1', 'value2', 'value3']
+      const expected = ['value1', 'value2', 'value3']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
 
     it('should handle non-unique values correctly', () => {
-      const input = { a: 'value', b: 'value', c: 'value' }
-      const expected = { a: 'value', b: 'value_1', c: 'value_2' }
+      const input = ['value', 'value', 'value']
+      const expected = ['value', 'value_1', 'value_2']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
 
     it('should handle empty values correctly', () => {
-      const input = { a: '', b: 'value', c: '' }
-      const expected = { a: 'empty', b: 'value', c: 'empty_1' }
+      const input = ['', 'value', '']
+      const expected = ['empty', 'value', 'empty_1']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
 
     it('should remove asterisks from values', () => {
-      const input = { a: 'val*ue', b: 'val*ue', c: 'val*ue' }
-      const expected = { a: 'value', b: 'value_1', c: 'value_2' }
+      const input = ['val*ue', 'val*ue', 'val*ue']
+      const expected = ['value', 'value_1', 'value_2']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
 
     it('should handle mixed cases of unique, non-unique, and empty values', () => {
-      const input = { a: 'value', b: '', c: 'value', d: 'unique', e: '' }
-      const expected = {
-        a: 'value',
-        b: 'empty',
-        c: 'value_1',
-        d: 'unique',
-        e: 'empty_1',
-      }
+      const input = ['value', '', 'value', 'unique', '']
+      const expected = ['value', 'empty', 'value_1', 'unique', 'empty_1']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
 
     it('should handle case sensitivity correctly', () => {
-      const input = { a: 'value', b: 'Value', c: 'value', d: 'VALUE' }
-      const expected = { a: 'value', b: 'Value', c: 'value_1', d: 'VALUE' }
+      const input = ['value', 'Value', 'value', 'VALUE']
+      const expected = ['value', 'Value', 'value_1', 'VALUE']
 
       expect(prependNonUniqueHeaderColumns(input)).toEqual(expected)
     })
