1cc8e4bf6eb7f1d49082719c6eab0d761d0adab2
Carl Brugger <carl.brugger@flatfile.io>
2025-02-19 12:16:52 -0600

    chore: formatting

--- plugins/space-configure/src/utils/data.checklist.ts
+++ plugins/space-configure/src/utils/data.checklist.ts
@@ -1,30 +1,37 @@
-import { FlatfileClient } from "@flatfile/api";
-import { FlatfileEvent } from "@flatfile/listener";
+import { FlatfileClient } from '@flatfile/api'
+import { FlatfileEvent } from '@flatfile/listener'
 
 const api = new FlatfileClient()
 
 export const createDataChecklist = async (event: FlatfileEvent) => {
-  const {spaceId} = event.context
-  
-  const {data: workbooks} = await api.workbooks.list({spaceId})
-  
-  let body = `<div class="my-doc">\n`;
+  const { spaceId } = event.context
 
-  for(const workbook of workbooks) {
-    const {data: sheets} = await api.sheets.list({workbookId: workbook.id})
-    body += `  <h2>${workbook.name}</h2>`;
-    
-    for(const sheet of sheets) {
-      const fields = sheet.config.fields;
-      const fieldTable = fields.map(field => {
-        const constraints = field.constraints?.map(constraint => `<span style="color: #d97a71; background-color: #fff0ef; padding: 2px 6px; border-radius: 5px; margin-left: 8px;">${constraint.type}</span>`).join('');
-        return `
+  const { data: workbooks } = await api.workbooks.list({ spaceId })
+
+  let body = `<div class="my-doc">\n`
+
+  for (const workbook of workbooks) {
+    const { data: sheets } = await api.sheets.list({ workbookId: workbook.id })
+    body += `  <h2>${workbook.name}</h2>`
+
+    for (const sheet of sheets) {
+      const fields = sheet.config.fields
+      const fieldTable = fields
+        .map((field) => {
+          const constraints = field.constraints
+            ?.map(
+              (constraint) =>
+                `<span style="color: #d97a71; background-color: #fff0ef; padding: 2px 6px; border-radius: 5px; margin-left: 8px;">${constraint.type}</span>`
+            )
+            .join('')
+          return `
   <tr>
     <td style="padding:8px; border-bottom:1px solid #f3f2f2;">${field.label}${constraints ?? ''}</td>
     <td style="padding:8px; border-bottom:1px solid #f3f2f2;"><span style="color: #2e424b; background-color: #f0f0f0; padding: 2px 6px; border-radius: 5px;">${field.type}</span></td>
     <td style="padding:8px; border-bottom:1px solid #f3f2f2;">${field.description || ''}</td>
-  </tr>`;
-      }).join('')
+  </tr>`
+        })
+        .join('')
 
       body += `
 <h3>${sheet.name}</h3>
@@ -38,14 +45,14 @@ export const createDataChecklist = async (event: FlatfileEvent) => {
 </thead>
 <tbody>${fieldTable}
 </tbody>
-</table>`;
+</table>`
     }
   }
 
-  body += `\n</div>`;
-  
+  body += `\n</div>`
+
   await api.documents.create(spaceId, {
     title: `Data Checklist`,
     body,
-  });
+  })
 }
