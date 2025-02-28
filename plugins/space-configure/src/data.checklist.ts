bbc4d2c3cd9391e899a9a00602d961e3c6ff2f3b
Carl Brugger <carl.brugger@flatfile.io>
2025-02-27 18:58:09 -0600

    filter out file workbooks

--- plugins/space-configure/src/data.checklist.ts
+++ plugins/space-configure/src/data.checklist.ts
@@ -1,13 +1,26 @@
 import type { Flatfile } from '@flatfile/api'
 import type { FlatfileEvent, FlatfileListener } from '@flatfile/listener'
 import { dataChecklist } from './utils/data.checklist'
+import { FlatfileClient } from '@flatfile/api'
+
+const api = new FlatfileClient()
 
 export function dataChecklistPlugin() {
   return function (listener: FlatfileListener) {
     listener.on(
       ['workbook:created', 'workbook:updated'],
       async (event: FlatfileEvent) => {
-        const { spaceId } = event.context
+        const { spaceId, workbookId } = event.context
+
+        const { data: workbook } = await api.workbooks.get(workbookId)
+        if (
+          workbook.labels.includes('file') &&
+          workbook.name.startsWith('[file]')
+        ) {
+          console.log('Skipping file workbook')
+          return
+        }
+
         console.log(
           `\`dataChecklist\` firing on \`${event.topic}\` for space \`${spaceId}\``
         )
