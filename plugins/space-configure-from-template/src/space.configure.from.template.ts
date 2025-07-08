947a877443d0d1fe40d014e7306649d5efcde12a
Jacqueline Mander <jacqueline@flatfile.io>
2025-07-08 11:46:13 -0400

    fix: add default obj for metadata

--- plugins/space-configure-from-template/src/space.configure.from.template.ts
+++ plugins/space-configure-from-template/src/space.configure.from.template.ts
@@ -67,7 +67,7 @@ export function configureSpaceFromTemplate(
               })),
               actions: workbook.actions,
               settings: workbook.settings,
-              metadata: workbook.metadata,
+              metadata: workbook.metadata ?? {},
               treatments: workbook.treatments,
             }))
 
@@ -86,7 +86,7 @@ export function configureSpaceFromTemplate(
             primaryWorkbookId:
               workbookIds && workbookIds.length > 0 ? workbookIds[0] : '',
             settings: spaceTemplate.settings || {},
-            metadata: spaceTemplate.metadata,
+            metadata: spaceTemplate.metadata || {},
             actions: spaceTemplate.actions || [],
             access: spaceTemplate.access || [],
             labels: spaceTemplate.labels,
